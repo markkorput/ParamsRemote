ce528dbf3a74e260700cc6c4fff6354339184094
mark <mark.korput@gmail.com>
2020-01-20 12:15:29 +0100

    session component shows list of params

--- src/app/app-session/app-session.component.ts
+++ src/app/app-session/app-session.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RemoteParamsService } from '../remote-params.service';
+import { Observable, of } from 'rxjs';
+
+class Param {
+  path: string;
+  type: string;
+  value: any;
+  opts: object;
+
+  constructor(path: string, type: string, value: any, opts: object) {
+    this.path = path;
+    this.type = type;
+    this.value = value;
+    this.opts = opts || {};
+  }
+}
 
 @Component({
   selector: 'app-app-session',
@@ -18,5 +33,13 @@ export class AppSessionComponent implements OnInit {
 
   disconnect() {
     this.remoteParamsService.disconnect(this.id);
+  }
+
+  getParams(): Observable<Param[]> {
+    return of([
+      new Param('/test/param', 's', undefined, undefined),
+      new Param('/test/param2', 'i', undefined, undefined),
+      new Param('/test/param3', 'f', undefined, undefined),
+      new Param('/test/param4', 'b', undefined, undefined)]);
   }
 }
